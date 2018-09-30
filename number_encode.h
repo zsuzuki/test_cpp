f0c05957ca736d14de60027c407854a5668bd9b7
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-10-01 06:45:59 +0900

    数値調整。

--- number_encode.h
+++ number_encode.h
@@ -14,17 +14,17 @@ std::pair<uint64_t, uint64_t>
 get_encode_number(int idx)
 {
   static const uint64_t encode_table[] = {
-      0x07c667b4, 0x0113a0c7, 0x059e95d5, 0x00f8906b, 0x06c19c92, 0x0492616c, 0x03fd9fac, 0x00ff7ab4, 0x0031a32e,
-      0x0451a094, 0x01cae570, 0x019abd2f, 0x07890755, 0x077483a3, 0x060b7bec, 0x0101b5e0, 0x024a79dc, 0x05d5e211,
-      0x00d49adf, 0x002a9014, 0x001d4d7e, 0x047f0b48, 0x039ee552, 0x04bfbdac, 0x03b96e2e, 0x07b19f81, 0x02c40a61,
-      0x056e07fe, 0x021ea4c0, 0x03d1bc1d, 0x02a5ac4c, 0x0577c6e4, 0x02f7c379,
+      0xfe9494af, 0x4d80291b, 0x83baf026, 0x57d54135, 0xb799056d, 0xeb23d7ef, 0x4dc2eb5b, 0xb384ac7e, 0xa60623b5,
+      0x7b230661, 0xe40f92b0, 0x020340db, 0xac136d61, 0x2aa0314d, 0xcedc6fca, 0x094369d2, 0x932ee623, 0x6271fc98,
+      0x416bf52f, 0xfb805cb3, 0x96bf1b47, 0x0763e831, 0x7dcddced, 0xe0250084, 0x0791188e, 0xa5b949c4, 0x7741375a,
+      0xa09dd89b, 0xd71ae460, 0xcca469f4, 0x6a759117, 0xb003a6fe, 0x0577dda0,
   };
-  return std::make_pair(encode_table[idx], encode_table[idx + 1]);
+  return std::make_pair(encode_table[idx], encode_table[idx + 1] & 0x07ffffffUL);
 }
 
 //
 uint64_t
-rotate(uint64_t n, uint64_t n_sh = 16)
+rotate(uint64_t n, uint64_t n_sh)
 {
   uint64_t m1 = (1ULL << n_sh) - 1;
   uint64_t m2 = 0x07ffffffffffffffULL ^ m1;
@@ -45,7 +45,7 @@ encode(uint32_t num)
   code ^= en.first;
   code *= en.second;
   code = rotate(code, s);
-  code = (code & 0xffffffff) << 32 | ((code >> 32) & 0x07ffffff) | (s << (32 - 5));
+  code = (code & 0xffffffffUL) << 32 | ((code >> 32) & 0x07ffffffUL) | (s << (32 - 5));
 
   return code;
 }
@@ -54,7 +54,7 @@ encode(uint32_t num)
 uint32_t
 decode(uint64_t code)
 {
-  uint64_t num = (code & 0x07ffffff) << 32 | ((code >> 32) & 0xffffffff);
+  uint64_t num = (code & 0x07ffffffUL) << 32 | ((code >> 32) & 0xffffffffUL);
   uint64_t s   = (code >> (32 - 5)) & 31;
   num          = rotate(num, 59 - s);
   auto en      = get_encode_number(s);
