2ae07558907a050304da15c764356ee11ea07c4d
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-11-12 01:41:08 +0000

    build for cloud9

--- atomic_queue.h
+++ atomic_queue.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <atomic>
+#include <cstddef>
 #include <vector>
 
 //
--- memcached.cpp
+++ memcached.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <string>
 #include <unistd.h>
+#include <cstdio>
 
 #include <arpa/inet.h>
 #include <netdb.h>
