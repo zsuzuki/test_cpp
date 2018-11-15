dbbc5c00cdd026e22bb5675dac57cbb7740b1a8a
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-11-15 23:25:07 +0900

    ファイルに対する入出力に対応させる。

--- zlib_test.cpp
+++ zlib_test.cpp
@@ -4,8 +4,10 @@
 
 #include <array>
 #include <cstdint>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "zlib.hpp"
 
@@ -53,11 +55,12 @@ public:
   };
 
 private:
-  buff src;
-  buff dst;
+  buff   src;
+  buff   dst;
+  size_t wsz;
 
 public:
-  zinp(const buff& i, const buff& o) : src(i), dst(o) {}
+  zinp(const buff& i, const buff& o) : src(i), dst(o), wsz(0) {}
   void*  getSource() override { return src.ptr; }
   void*  getDestination() override { return dst.ptr; }
   size_t getRemainSize() const override { return src.sz; }
@@ -69,8 +72,14 @@ public:
     std::cout << "read: " << rsize << ", write: " << wsize << std::endl;
     return src.sz == 0;
   }
-  void complete(bool ok, size_t tws) override { std::cout << "write: " << tws << std::endl; }
+  void complete(bool ok, size_t tws) override
+  {
+    std::cout << "write: " << tws << ", " << (ok ? "SUCCESS" : "FAILES") << std::endl;
+    wsz = tws;
+  }
   void error() override { std::cerr << "error!" << std::endl; }
+
+  size_t getTotalSize() const { return wsz; }
 };
 } // namespace
 
@@ -78,22 +87,56 @@ public:
 int
 main(int argc, char** argv)
 {
-  using Buffer = std::array<Bytef, 1024>;
-  Buffer     buffer;
-  Buffer     testbuff;
+  std::string ifname, ofname;
+
+  bool uncompress = false;
+  for (int i = 1; i < argc; i++)
+  {
+    std::string a = argv[i];
+    if (a == "-d")
+      uncompress = true;
+    else if (ifname.empty())
+      ifname = a;
+    else if (ofname.empty())
+      ofname = a;
+  }
+  if (ifname.empty() || ofname.empty())
+  {
+    std::cerr << "no input filename." << std::endl;
+    return 1;
+  }
+
+  std::ifstream ifs{ifname, std::ios::binary | std::ios::ate};
+  if (ifs.good() == false)
+  {
+    std::cerr << "open error<input>: " << ifname << std::endl;
+    return 1;
+  }
+  std::ofstream ofs{ofname, std::ios::binary};
+  if (ofs.good() == false)
+  {
+    std::cerr << "open error<output>: " << ofname << std::endl;
+    return 1;
+  }
+  auto isz = ifs.tellg();
+  ifs.seekg(0);
+  std::cout << "input file size: " << isz << std::endl;
+  std::vector<Bytef> ibuffer;
+  ibuffer.resize(isz);
+  ifs.read((char*)ibuffer.data(), ibuffer.size());
+
+  std::vector<Bytef> obuffer;
+  obuffer.resize(isz * 4);
+
   ZLIB::zlib zlib;
-  zinp::buff isrc{msg, sizeof(msg)}, idst{buffer};
+  zinp::buff isrc{ibuffer}, idst{obuffer};
   zinp       zi{isrc, idst};
-  zlib.compress(zi);
-  std::cout << "deflate work-size: " << zlib.getWorkSize() << std::endl;
-
-  zinp::buff osrc{buffer}, odst{testbuff};
-  zinp       zo{osrc, odst};
-  zlib.uncompress(zo);
-  std::cout << "inflate work-size: " << zlib.getWorkSize() << std::endl;
-
-  std::cout << testbuff.data() << std::endl;
-  std::cout << "done." << std::endl;
+  if (uncompress)
+    zlib.uncompress(zi);
+  else
+    zlib.compress(zi);
+  std::cout << "work-size: " << zlib.getWorkSize() << std::endl;
+  ofs.write((char*)obuffer.data(), zi.getTotalSize());
 
   return 0;
 }
