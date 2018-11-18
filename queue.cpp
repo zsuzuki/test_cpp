cfc780c9f4a6d5911e61010a08cd214b35dbbc0b
Yoshinori Suzuki <wave.suzuki.z@gmail.com>
2018-11-18 17:29:31 +0900

    数値キューテスト。

--- queue.cpp
+++ queue.cpp
@@ -6,8 +6,9 @@ main(int argc, char** argv)
 {
   std::vector<int> v;
   Queue<int>       q(20);
-  int cnt = 0;
+  int              cnt = 0;
 
+  std::cout << "Pointer queue" << std::endl;
   v.resize(10);
   for (int l = 0; l < 4; l++)
   {
@@ -26,6 +27,23 @@ main(int argc, char** argv)
     }
     std::cout << (q.empty() ? "EMPTY" : "-") << std::endl;
   }
+
+  std::cout << "Number queue" << std::endl;
+  NQueue<int> nq{10, -1};
+  for (int l = 0; l < 4; l++)
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      auto n = l * 10 + i;
+      if (nq.push(n) == false)
+        std::cout << "failed push: " << n << std::endl;
+    }
+    while (auto r = nq.pop())
+    {
+      std::cout << r.value << std::endl;
+    }
+  }
+  std::cout << "done." << std::endl;
 
   return 0;
 }
