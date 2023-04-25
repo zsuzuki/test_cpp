d7d3b5a23748bf8e25ef1913f7b07d2f2e38cda6
zsuzuki <wave.suzuki.z@gmail.com>
2023-04-26 01:17:04 +0900

    スレッドテスト追記。

--- thread_test2.cpp
+++ thread_test2.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <vector>
 
+namespace
+{
+//
 std::atomic_int keeper{100};
-
-int
-main(int argc, char** argv)
+void
+test1()
 {
   class testWorker : public SimpleWorker::Worker
   {
@@ -51,6 +53,75 @@ main(int argc, char** argv)
   }
 
   std::cout << count.load() << std::endl;
+}
+
+//
+class ChildWorker : public SimpleWorker::Worker
+{
+public:
+  void run() override { std::cout << "child" << std::endl; }
+};
+class CheckWorker : public SimpleWorker::Worker
+{
+public:
+  void run() override { std::cout << "check" << std::endl; }
+};
+std::atomic_int          mainCount{};
+std::atomic_int          childCount{};
+std::vector<ChildWorker> children;
+CheckWorker              wkCheck;
+void
+test2()
+{
+  class MainWorker : public SimpleWorker::Worker
+  {
+    SimpleWorker::WorkerThread& wt;
+
+  public:
+    MainWorker(SimpleWorker::WorkerThread& w) : wt(w) {}
+    void run() override
+    {
+      std::cout << "start" << std::endl;
+
+      children.resize(100);
+      childCount = children.size();
+
+      mainCount.fetch_add(1);
+      wt.push(&childCount, wkCheck, &mainCount);
+
+      for (auto& ch : children)
+      {
+        wt.push(ch, &childCount);
+      }
+    }
+  };
+  class PostWorker : public SimpleWorker::Worker
+  {
+  public:
+    void run() override { std::cout << "post" << std::endl; }
+  };
+
+  SimpleWorker::WorkerThread wt{};
+
+  MainWorker wkMain{wt};
+  PostWorker wkPost;
+
+  mainCount = 1;
+  wt.push(&mainCount, wkPost);
+  wt.push(wkMain, &mainCount);
+
+  wt.execute();
+  wt.wait();
+}
+
+} // namespace
+
+//
+int
+main(int argc, char** argv)
+{
+  test1();
+  test2();
 
   return 0;
 }
